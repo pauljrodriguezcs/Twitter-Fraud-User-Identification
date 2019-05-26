76f580ff1c8d5d803bc761b0b87bd2fe973795cf
pauljrodriguezcs <prodr010@ucr.edu>
2019-05-25 17:18:00 -0700

    all graphs finished

--- prog.py
+++ prog.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 def main():
 	### Part 1: reads in file and plots sparsity ###
+	### 1)
 	print("importing file ...")
 	gr = np.loadtxt('assignment_graph.txt',delimiter=',',dtype=int)
 	print("done ...")
@@ -23,31 +24,36 @@ def main():
 	adj_matrix = sparse.csr_matrix((ones,(gr[:,0],gr[:,1])),shape=s,dtype=float)
 	print("done ...")
 
+	### 3)
 	# print("creating spy plot...")
 	# plt.title("Sparsity Graph")
 	# plt.spy(adj_matrix,marker='.',markersize=1)
 	# plt.show()
 
 	### Part 2: finds the degree and plots distribution vs node count ###
+	### 1)
+	print("\nfinding degree distribution ...")
+	# map with where key value is the degree of node and value is the count 
+	deg_dist = {}
+	node_sum = []
+	for i in range(s[0]):
+		deg = adj_matrix.getrow(i).sum(axis=1).sum()
+		node_sum.append(deg)
+		if((deg in deg_dist) and deg > 0):
+			deg_dist[deg] += 1
 
-	# print("\nfinding degree distribution ...")
-	# deg_dist = {}
-	# for i in range(s[0]):
-	# 	deg = adj_matrix.getrow(i).sum(axis=1).sum()
-	# 	if((deg in deg_dist) and deg > 0):
-	# 		deg_dist[deg] += 1
+		else:
+			deg_dist[deg] = 1
 
-	# 	else:
-	# 		deg_dist[deg] = 1
+	node_deg = list(deg_dist.keys())
+	node_cnt = []
 
-	# node_deg = list(deg_dist.keys())
-	# node_cnt = []
+	for i in node_deg:
+		node_cnt.append(deg_dist.get(i))
 
-	# for i in node_deg:
-	# 	node_cnt.append(deg_dist.get(i))
+	print("done ...")
 
-	# print("done ...")
-
+	### 2)
 	# print("plotting degree distribution ...")
 	# plt.loglog(node_deg,node_cnt,'b.')
 	# plt.title("degree distribution")
@@ -55,9 +61,16 @@ def main():
 	# plt.ylabel("node count")
 	# plt.show()
 
-	### Part 3: singular value decomposition ###
+	### 4)
+	# plt.plot(node_sum,'bo',markersize=1)
+	# plt.title("abnormal nodes")
+	# plt.xlabel("node")
+	# plt.ylabel("degree")
+	# plt.xticks(np.arange(0,len(node_sum),1000))
+	# plt.show()
 
-	# a) find k that reproduces 90% of original data
+	### Part 3: singular value decomposition ###
+	### 2a) find k that reproduces 90% of original data
 	# k = 80
 	# p_data = 0.0
 
@@ -91,7 +104,7 @@ def main():
 
 	top_k = 5
 
-	# b) 
+	### 2b) 
 	# print("\nsolving svds for k =", top_k)
 	# U, S, V = spl.svds(adj_matrix,k=top_k)
 	# print("plotting ... " )
@@ -107,47 +120,44 @@ def main():
 	# plt.legend(('LSV - 1','LSV - 2','LSV - 3','LSV - 4','LSV - 5'),loc='best')
 	# plt.show()
 
-	# d)
-	
-	print("\nsolving svds for k =", top_k)
-	U, S, V = spl.svds(adj_matrix,k=top_k)
+	### 2d)
+	# print("\nsolving svds for k =", top_k)
+	# U, S, V = spl.svds(adj_matrix,k=top_k)
 
-	u_abs = np.array(U)
-	u_abs = np.absolute(np.array(U))
+	# u_abs = np.array(U)
+	# u_abs = np.absolute(np.array(U))
 
-	indices = np.ones([100,top_k],dtype=int)
+	# indices = np.ones([100,top_k],dtype=int)
 
-	print("finding top 100 ...")
-	for i in range(top_k):
-		ind = np.argpartition(u_abs[:,i], -100)[-100:]
-		indices[:,i] = ind[np.argsort(u_abs[:,i][ind])]
+	# print("finding top 100 ...")
+	# for i in range(top_k):
+	# 	ind = np.argpartition(u_abs[:,i], -100)[-100:]
+	# 	indices[:,i] = ind[np.argsort(u_abs[:,i][ind])]
 
-	u_top = np.zeros(U.shape)
-	v_top = np.zeros(V.shape)
+	# u_top = np.zeros(U.shape)
+	# v_top = np.zeros(V.shape)
 
-	print("obtaining top 100 ...")
-	for i in range(top_k):
-		for j in range(indices.shape[0]):
-			ind = indices[j,i]
-			u_top[ind,i] = U[ind,i]
-			v_top[i,ind] = V[i,ind]
+	# print("obtaining top 100 ...")
+	# for i in range(top_k):
+	# 	for j in range(indices.shape[0]):
+	# 		ind = indices[j,i]
+	# 		u_top[ind,i] = U[ind,i]
+	# 		v_top[i,ind] = V[i,ind]
 
-	print("graphing top 100 ...")
-	for i in range(5):
-		plt.subplot(2,3,(i+1))
-		plt.title('Left Singular Vector #%i' % (i+1))
+	# print("graphing top 100 ...")
+	# for i in range(5):
+	# 	plt.subplot(2,3,(i+1))
+	# 	plt.title('Matrix #%i' % (i+1))
+	# 	m = np.reshape(S[i] * u_top[:,i],(u_top.shape[0],1)) * v_top[i,:]
+	# 	plt.spy(m,markersize=1)
 
-		m = np.reshape(S[i] * u_top[:,i],(u_top.shape[0],1))
-		m = m @ np.reshape(v_top[i,:],(1,v_top.shape[1]))
-		plt.spy(m,markersize=1)
-
-	plt.subplot(2,3,6)
-	plt.title('Left Singular Vector Top 5')
-	s_dag = np.diag(S)
-	m_top = u_top @ s_dag @ v_top
-	print(m_top.shape)
-	plt.spy(m_top,markersize=1)
-	plt.show()
+	# plt.subplot(2,3,6)
+	# plt.title('Matrix Top 5')
+	# s_dag = np.diag(S)
+	# m_top = u_top @ s_dag @ v_top
+	# print(m_top.shape)
+	# plt.spy(m_top,markersize=1)
+	# plt.show()
 	
 	
 if __name__=="__main__":
